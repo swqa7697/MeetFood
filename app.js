979518ad50846d25c89f7533cb813148e09aaff2
swqa7697 <swqa7697@gmail.com>
2024-02-12 10:52:26 -0800

    Add app.js

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const config = require('./config/production.json');
+const port = process.env.PORT || 3000;
+const expressValidator = require('express-validator');
+
+mongoose.set('debug', true);
+
+// Routes
+//
+
+const app = express();
+
+// Middleware
+app.use(
+  express.json({
+    verify: (req, res, buf) => {
+      req.rawBody = buf;
+    },
+  }),
+);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
+
+// Add routers
+app.get('/', function (req, res) {
+  res.status(200).json({
+    message: 'Successfully access MeetFood API.',
+  });
+});
+
+// Database
+mongoose
+  .connect(config.mongodbConnectURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: config.dbName,
+  })
+  .then(() => {
+    console.log('Database Connection is ready...');
+    app.listen(port);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+module.exports = app;
